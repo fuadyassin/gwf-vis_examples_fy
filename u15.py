55f76c40b6b06522b247efe5841a7247060e2979
Sean Wong <seanwong24@live.com>
2023-08-18 19:41:44 +0000

    add u15.py

---
+++ u15.py
@@ -0,0 +1,44 @@
+# %% import
+from gwfvis import core as gwfvis_core, default as gwfvis_default
+
+# %% config
+vis_config = gwfvis_default.create_recommended_config()
+gwfvis_core.set_view(vis_config, center=[51, -115], zoom=5)
+
+# %% setup data provider
+data_provider_plugin = gwfvis_core.add_plugin(
+    vis_config, name='gwf-default.sqlite-local-data-provider')
+data_provider_plugin = gwfvis_core.add_plugin(
+    vis_config, name='gwf-default.gwfvisdb-data-provider')
+
+# %% add U15 layer
+data_source = 'gwfvisdb:https://gwf-vis.usask.ca/v1/api/file/fetch/public/datasets/u15.gwfvisdb'
+u15_layer = gwfvis_core.add_plugin(
+    config=vis_config, name='gwf-default.geojson-layer')
+gwfvis_core.set_plugin_props(
+    u15_layer,
+    {
+        'displayName': 'U15',
+        'layerType': 'overlay',
+        'active': True
+    }
+)
+# %% add data control
+data_control = gwfvis_core.add_plugin(
+    config=vis_config, name='gwf-default.data-control', container='main', props={
+        'dataSources': ['gwfvisdb:https://gwf-vis.usask.ca/v1/api/file/fetch/public/datasets/u15.gwfvisdb'],
+        'dataSourceDict': {'U15': 'gwfvisdb:https://gwf-vis.usask.ca/v1/api/file/fetch/public/datasets/u15.gwfvisdb'}
+    }
+)
+
+# %% add metadata
+metadata = gwfvis_core.add_plugin(
+    config=vis_config, name='gwf-default.metadata', container='sidebar', container_props={'slot': 'top'})
+
+# %% option1: print the config JSON
+print(vis_config)
+
+# %% option2: print the URL
+print(gwfvis_core.generate_vis_url(vis_config))
+
+# %%

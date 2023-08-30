68d384e191f2f92bdda18de0dc3c533e1855699f
Sean Wong <seanwong24@live.com>
2023-08-30 08:59:54 +0000

    update imports

--- u15.py
+++ u15.py
@@ -1,21 +1,22 @@
 # %% import
-from gwfvis import core as gwfvis_core, default as gwfvis_default
+import vga
+import gwfvis
 
 # %% config
-vis_config = gwfvis_default.create_config()
-gwfvis_core.set_view(vis_config, center=[51, -115], zoom=5)
+vis_config = gwfvis.create_config()
+vga.set_view(vis_config, center=[51, -115], zoom=5)
 
 # %% setup data provider
-data_provider_plugin = gwfvis_core.add_plugin(
-    vis_config, name=gwfvis_default.PluginNames.SQLITE_LOCAL_DATA_PROVIDER)
-data_provider_plugin = gwfvis_core.add_plugin(
-    vis_config, name=gwfvis_default.PluginNames.GWFVISDB_DATA_PROVIDER)
+data_provider_plugin = vga.add_plugin(
+    vis_config, name=gwfvis.PluginNames.SQLITE_LOCAL_DATA_PROVIDER)
+data_provider_plugin = vga.add_plugin(
+    vis_config, name=gwfvis.PluginNames.GWFVISDB_DATA_PROVIDER)
 
 # %% add U15 layer
 data_source = 'gwfvisdb:https://gwf-vis.usask.ca/v1/api/file/fetch/public/datasets/u15.gwfvisdb'
-u15_layer = gwfvis_core.add_plugin(
-    config=vis_config, name=gwfvis_default.PluginNames.GEOJSON_LAYER)
-gwfvis_core.set_plugin_props(
+u15_layer = vga.add_plugin(
+    config=vis_config, name=gwfvis.PluginNames.GEOJSON_LAYER)
+vga.set_plugin_props(
     u15_layer,
     {
         'displayName': 'U15',
@@ -25,21 +26,21 @@ gwfvis_core.set_plugin_props(
     }
 )
 # %% add data control
-data_control = gwfvis_core.add_plugin(
-    config=vis_config, name=gwfvis_default.PluginNames.DATA_CONTROL, container='main', props={
+data_control = vga.add_plugin(
+    config=vis_config, name=gwfvis.PluginNames.DATA_CONTROL, container='main', props={
         'dataSources': ['gwfvisdb:https://gwf-vis.usask.ca/v1/api/file/fetch/public/datasets/u15.gwfvisdb'],
         'dataSourceDict': {'U15': 'gwfvisdb:https://gwf-vis.usask.ca/v1/api/file/fetch/public/datasets/u15.gwfvisdb'}
     }
 )
 
 # %% add metadata
-metadata = gwfvis_core.add_plugin(
-    config=vis_config, name=gwfvis_default.PluginNames.METADATA, container='sidebar', container_props={'slot': 'top'})
+metadata = vga.add_plugin(
+    config=vis_config, name=gwfvis.PluginNames.METADATA, container='sidebar', container_props={'slot': 'top'})
 
 # %% option1: print the config JSON
 print(vis_config)
 
 # %% option2: print the URL
-print(gwfvis_core.generate_vis_url(vis_config))
+print(vga.generate_vis_url(vis_config))
 
 # %%

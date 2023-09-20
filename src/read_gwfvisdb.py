5f905d58d6fbb618f69927d120b540aed44af6c2
Sean Wong <seanwong24@live.com>
2023-09-20 19:04:33 +0000

    add read_gwfvisdb.py

---
+++ src/read_gwfvisdb.py
@@ -0,0 +1,33 @@
+# %%
+# ! pip install git+https://github.com/vga-team/python-helper.git
+# ! pip install git+https://github.com/vga-team/gwf-vis-python-helper.git
+
+# %% import
+from gwfvis import db as gwfvisdb
+import json
+import urllib.request
+
+#%% download the gwfvisdb file
+gwfvisdb_url = 'https://gwf-vis.usask.ca/v1/api/file/fetch/public/datasets/mesh.new.gwfvisdb'
+gwfvisdb_path = '_.gwfvisdb'
+urllib.request.urlretrieve(gwfvisdb_url, gwfvisdb_path)
+
+# %% read the db file
+options = gwfvisdb.read_gwfvis_db(gwfvisdb_path)
+
+#%% info
+options.info
+
+# %% locations
+options.locations
+
+#%% dimensions
+options.dimensions
+
+#%% variables
+options.variables
+
+#%% values
+options.values
+
+#%%
